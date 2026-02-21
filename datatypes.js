5684c3a9bdf405a26409b9257043bf12a367f299
Ajay <ajay123@gmail.com>
2026-02-21 23:00:28 +0530

    Added JS files

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+// "use strcit;"//  treat all js  code as newer  version
+
+// // alert(3+3) we are using nodejs,not browser
+// // code readability should be high
+// let name ="ajay"
+// let age =18
+// let isloggecIn =true
+
+
+// number => 2 to pwer 53 
+// bignit
+// string =>""
+// boolean=>true/false
+//null =>standalone value
+// undefined =>
+// symbol=> unique
+
+// object
+
+
+console.log(typeof null);
---
+++ variable.js
@@ -0,0 +1,16 @@
+const accountID = 144553
+let accountEmail = "ak6722495@gmail.com"
+var accountPassword = "12345"
+let accountCity = "jaipur"
+
+accountEmail = "advs.com"
+accountPassword = "21212121"
+accountCity = "punjab"
+let accountstate;
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity,accountstate]);
+
+/*
+prefer not to use var
+becuae of issue in block scope and functional scope
+*/
